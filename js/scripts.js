e1ac65d8c7706468788aaf8c0ea2b7dc12bd4a0d
Danny Pineda and Sarah Read Brown <dcorn13@gmail.com>
2016-03-21 19:35:43 -0700

    Hide input form after submit

--- js/scripts.js
+++ js/scripts.js
@@ -16,20 +16,20 @@ $(document).ready(function() {
 
     var newAccount = new BankAccount (inputtedFullName, inputtedAccountAlias, inputtedAccountType, inputtedInitialDeposit);
 
-    $("ul#accounts").append("<li><span class='account'>" + newAccount.alias + " (" + newAccount.type + ")" + "</span></li>");
+    // $("ul#accounts").append("<li><span class='account'>" + newAccount.alias + " (" + newAccount.type + ")" + "</span></li>");
 
-    $(".account").last().click(function() {
-      $("#show-account").show();
-      $("#show-account h2").text(newAccount.alias);
-      $(".full-name").text(newAccount.name);
-      $(".account-type").text(newAccount.type);
-      $(".account-balance").text(newAccount.initial);
-    });
-
-    $("input#new-name").val("");
-    $("input#new-account-type").val("");
-    $("input#new-initial-deposit").val("");
-    $("input#new-account-alias").val("");
+    // $(".account").last().click(function() {
+    $("#show-account").show();
+    $(".account-alias").text(newAccount.alias);
+    $(".full-name").text(newAccount.name);
+    $(".account-type").text(newAccount.type);
+    $(".account-balance").text(newAccount.initial);
+    // });
+    $(".add-account").hide();
+    // $("input#new-name").val("");
+    // $("input#new-account-type").val("");
+    // $("input#new-initial-deposit").val("");
+    // $("input#new-account-alias").val("");
 
   });
 });
